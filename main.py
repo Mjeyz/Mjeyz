6c270f4f0df284b87fababbee3b84e09310df68b
Mudasir Jamshaid <thisismjeyz@gmail.com>
2024-11-26 09:00:45 +0400

    day 11 practice question Blackjack game

--- main.py
+++ main.py
@@ -1,53 +1,74 @@
-import art
+import random
+
+def deal_cards():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
 
 
-def add(n1, n2):
-    return n1 + n2
+def calculate_score(cards):
+    # Check for a blackjack (a hand with two cards: ace + 10)
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0  # 0 represents a blackjack
 
+    # Replace ace (11) with 1 if the score exceeds 21
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
 
-def subtract(n1, n2):
-    return n1 - n2
+    return sum(cards)
 
+def compare(u_score, c_score):
+    if u_score == c_score:
+        return "Draw 😊"
+    elif c_score == 0:
+        return "Lose opponent has a Blackjack 😢"
+    elif u_score == 0:
+        return "Win with Blackjack 😍"
+    elif u_score > 21:
+        return "You went over. you lose 😩"
+    elif c_score > 21:
+        return "Opponent went over. you win 🫡"
+    elif u_score > c_score:
+        return "You win 👌"
+    else:
+        return "You lose 😩😭"
 
-def multiply(n1, n2):
-    return n1 * n2
+# Game initialization
+def play_game():
+    user_cards = []
+    computer_cards = []
+    computer_score = -1
+    user_score = -1
+    is_game_over = False
 
+    for _ in range(2):
+        user_cards.append(deal_cards())
+        computer_cards.append(deal_cards())
 
-def divide(n1, n2):
-    return n1 / n2
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
 
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
 
-operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide,
-}
-
-# print(operations["*"](4, 8))
-
-
-def calculator():
-    print(art.logo)
-    should_accumulate = True
-    num1 = float(input("What is the first number?: "))
-
-    while should_accumulate:
-        for symbol in operations:
-            print(symbol)
-        operation_symbol = input("Pick an operation: ")
-        num2 = float(input("What is the next number?: "))
-        answer = operations[operation_symbol](num1, num2)
-        print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
-
-        if choice == "y":
-            num1 = answer
+        # Check for blackjack or bust
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
         else:
-            should_accumulate = False
-            print("\n" * 20)
-            calculator()
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass : ")
+            if user_should_deal == 'y':
+                user_cards.append(deal_cards())
+            else:
+                is_game_over = True
 
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_cards())
+        computer_score = calculate_score(computer_cards)
 
-calculator()
+    print(f"Your final hand : {user_cards}, final score : {user_score}")
+    print(f"Computer final hand : {computer_cards}, final score {computer_score}")
+    print(compare(user_score, computer_score))
+
+while input("Do you want play game of Blackjack? Type 'y' or 'n' : ") == "y":
+    play_game()
