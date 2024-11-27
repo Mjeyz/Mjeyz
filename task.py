2c86ec6a9040c91c46f2930436f9eda454b123ea
Mudasir Jamshaid <thisismjeyz@gmail.com>
2024-11-27 07:30:49 +0400

    day 12 practice question number guessing  game

--- task.py
+++ task.py
@@ -1,2 +1,42 @@
-f = open("Text.txt", "r")
-print(f.read())
+import random
+
+random_number = random.randint(1, 100)
+print("Welcome to Number Guessing Game! ")
+print("I'm thingking of number between 1 and 100.")
+choose_level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+if choose_level == "easy":
+    print("You have 10 attempts remaining to guess a number.")
+    for _ in range(10):
+        guess = int(input("Make a guess : "))
+        if guess > random_number:
+            print("Too high")
+            print("Guess again.")
+        elif guess < random_number:
+            print("Too low.")
+            print("Guess again.")
+        elif guess == random_number:
+            print("Congratulation 🫡 you win. ")
+            break
+        else:
+            print("inValid Number.")
+    else:
+        print("sorry! your attempts has finished. and you lose 😭")
+
+if choose_level == "hard":
+    print("You have 5 attempts remaining to guess a number.")
+    for _ in range(5):
+        guess = int(input("Make a guess : "))
+        if guess > random_number:
+            print("Too high")
+            print("Guess again.")
+        elif guess < random_number:
+            print("Too low.")
+            print("Guess again.")
+        elif guess == random_number:
+            print("Congratulation 🫡 you win. ")
+            break
+        else:
+            print("inValid Number.")
+    else:
+        print("sorry! your attempts has finished. and you lose 😭")
