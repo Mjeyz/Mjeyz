a21eedce26fc485d35296d901426f39bae5f1859
Mudasir Jamshaid <thisismjeyz@gmail.com>
2024-12-01 11:57:18 +0400

    PrettyTables

--- task.py
+++ task.py
@@ -1,42 +1,7 @@
-import random
+from prettytable import PrettyTable
 
-random_number = random.randint(1, 100)
-print("Welcome to Number Guessing Game! ")
-print("I'm thingking of number between 1 and 100.")
-choose_level = input("Choose a difficulty. Type 'easy' or 'hard': ")
-
-if choose_level == "easy":
-    print("You have 10 attempts remaining to guess a number.")
-    for _ in range(10):
-        guess = int(input("Make a guess : "))
-        if guess > random_number:
-            print("Too high")
-            print("Guess again.")
-        elif guess < random_number:
-            print("Too low.")
-            print("Guess again.")
-        elif guess == random_number:
-            print("Congratulation 🫡 you win. ")
-            break
-        else:
-            print("inValid Number.")
-    else:
-        print("sorry! your attempts has finished. and you lose 😭")
-
-if choose_level == "hard":
-    print("You have 5 attempts remaining to guess a number.")
-    for _ in range(5):
-        guess = int(input("Make a guess : "))
-        if guess > random_number:
-            print("Too high")
-            print("Guess again.")
-        elif guess < random_number:
-            print("Too low.")
-            print("Guess again.")
-        elif guess == random_number:
-            print("Congratulation 🫡 you win. ")
-            break
-        else:
-            print("inValid Number.")
-    else:
-        print("sorry! your attempts has finished. and you lose 😭")
+table = PrettyTable()
+table.add_column("Names", ["Mudasir", "Sawera", "Gulshen,"])
+table.add_column("Type", ["Student", "Student", "Housewife"])
+table.align = "l"
+print(table)
